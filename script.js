903dd36c61a33464c2f7bb786b16fe14ff98920b
Dumitru Vlad <vlad.dumitru1006@stud.fils.upb.ro>
2024-06-24 09:20:32 +0200

    Initial Commit of files

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginBtn = document.getElementById('loginBtn');
+    const signupBtn = document.getElementById('signupBtn');
+    const loginForm = document.getElementById('loginForm');
+    const signupForm = document.getElementById('signupForm');
+
+    loginBtn.addEventListener('click', () => {
+        loginBtn.classList.add('active');
+        signupBtn.classList.remove('active');
+        loginForm.classList.remove('hidden');
+        signupForm.classList.add('hidden');
+    });
+
+    signupBtn.addEventListener('click', () => {
+        signupBtn.classList.add('active');
+        loginBtn.classList.remove('active');
+        signupForm.classList.remove('hidden');
+        loginForm.classList.add('hidden');
+    });
+});
